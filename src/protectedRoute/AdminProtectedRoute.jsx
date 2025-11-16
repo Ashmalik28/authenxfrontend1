a16ec39c82603ed7a6e5a1256441fa83064ca617
Ashyam Malik <137030701+Ashmalik28@users.noreply.github.com>
2025-11-16 22:06:05 +0530

    Change Admin localStorage values to strings

--- src/protectedRoute/AdminProtectedRoute.jsx
+++ src/protectedRoute/AdminProtectedRoute.jsx
@@ -11,13 +11,13 @@ const AdminCheck = () => {
                 const accounts = await window.ethereum.request({method : "eth_accounts"});
 
                 if(accounts.length > 0 && typeof owner === "string" && accounts[0].toLowerCase() === owner.toLowerCase()){
-                    localStorage.setItem("Admin" , true );
+                    localStorage.setItem("Admin" , "true" );
                 }else {
-                    localStorage.setItem("Admin" , false);
+                    localStorage.setItem("Admin" , "false");
                 }
             } catch (error){
                 console.log("Admin check failed: " , error);
-                localStorage.setItem("Admin" , false);
+                localStorage.setItem("Admin" , "false");
                 
             }
         }
