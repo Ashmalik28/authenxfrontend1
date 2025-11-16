4d95d969f38949ad998aac1248bb9943543c8f41
Ashyam Malik <137030701+Ashmalik28@users.noreply.github.com>
2025-11-16 20:53:04 +0530

    Update API baseURL to include HTTPS

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL : "authenx.up.railway.app"
+    baseURL : "https://authenx.up.railway.app"
 });
 
 API.interceptors.request.use((config) => {
